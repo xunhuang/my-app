204ced044588b1922d6d082a45b01645d81e71ec
Wilson Huang (Growth) <xhuang@fb.com>
2021-01-12 16:16:46 -0800

    Updates

---
+++ precache-manifest.c3fa8a096f9d80e3b8eef5f722c3d23d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc014d1e89e67d2bf9f643da7bba865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f213e5e453a7ea5fd576",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "94c996070057ee815f76",
+    "url": "/static/js/2.d52af721.chunk.js"
+  },
+  {
+    "revision": "a9d7d1f9f042ec451e0a83a1d3c63c5d",
+    "url": "/static/js/2.d52af721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f213e5e453a7ea5fd576",
+    "url": "/static/js/main.0ac93874.chunk.js"
+  },
+  {
+    "revision": "366ddaea945e197457a1",
+    "url": "/static/js/runtime-main.ad264d5c.js"
+  }
+]);
